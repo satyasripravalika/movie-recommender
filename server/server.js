b641cdeaf326c983ed5e092e19076aa66dbc9c64
satyasripravalika <satyasripravalika@gmail.com>
2026-08-06 15:03:21 +0530

    Updated server for Render deployment

--- server/server.js
+++ server/server.js
@@ -8,4 +8,8 @@ app.use(express.json());
 
 app.use('/api/movies', require('./routes/movies'));
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
